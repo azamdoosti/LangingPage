1c6174bebf19245d088a0f4b6e0704d946e28b36
doosti <a.doosti1@gmail.com>
2024-10-27 17:23:02 +0330

    features

---
+++ src/components/features/data.js
@@ -0,0 +1,37 @@
+ import {FaAccessibleIcon,FaGg} from "react-icons/fa"
+ import {SiDatabricks} from "react-icons/si"
+ import {MdConnectWithoutcontatc} from "react-icons/md"
+
+ export const featuresList=[
+    {
+        id:1,
+        icon : <FaAccessibleIcon color="#0a1930" size={22}/>,
+        heading : "این برای شما طراحی شده",
+        text: "لورم ایپسوم ساختگی سادگی نامفهوم صنعت چاپ"
+    },
+    {
+        id:2,
+        icon : <SiDatabricks color="#0a1930" size={22}/>,
+        heading : "تاریخچه خود را نگهدارید",
+        text: "لورم ایپسوم ساختگی سادگی نامفهوم صنعت چاپ"
+    },
+    {
+        id:3,
+        icon : <MdConnectWithoutcontatc color="#0a1930" size={22}/>,
+        heading : "در ارتباط باش",
+        text: "لورم ایپسوم ساختگی سادگی نامفهوم صنعت چاپ"
+    },
+    {
+        id:4,
+        icon : <FaGg color="#0a1930" size={22}/>,
+        heading : "این برای شما طراحی شده",
+        text: "لورم ایپسوم ساختگی سادگی نامفهوم صنعت چاپ"
+    },
+    {
+        id:1,
+        icon : <FaAccessibleIcon color="#0a1930" size={22}/>,
+        heading : "کنترل را به دست گرفتن",
+        text: "لورم ایپسوم ساختگی سادگی نامفهوم صنعت چاپ"
+    },
+   
+ ]
