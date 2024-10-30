26774d5912171b8201bfc101373af7bd0856d58e
doosti <a.doosti1@gmail.com>
2024-10-30 17:04:41 +0330

    start subscribe

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
 const Subscribe = () => {
-  return <div>اشتراک</div>;
+  return (
+    <section id='subscribe'>
+     <div className='container subscribe'>
+      <h2>حالا مشترک شوید</h2>
+        <form>
+          <button>مشترک شوید</button>
+          <input type='text' placeholder='ایمیل خود را وارد کنید...'></input>
+        </form>
+        <div className='socials'>
+          <div className='social-icon'>
+           
+            <FaInstagram/>
+           
+          </div>
+          <div className='social-icon'>
+            
+            <FaTwitter/>
+           
+          </div>
+          <div className='social-icon'>
+           
+            <FaFacebook/>
+            
+          </div>
+          <div className='social-icon'>
+       
+            <TiSocialGooglePlus/>
+          
+          </div>
+        </div>
+     </div>
+    </section>
+  )
 };
 
 export default Subscribe;
