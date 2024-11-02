cbb38089d13961661214c75146b0220e1218ce65
doosti <a.doosti1@gmail.com>
2024-11-02 17:14:41 +0330

    subscribe complete style

--- src/components/subscribe/Subscribe.js
+++ src/components/subscribe/Subscribe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Subscribe.css'
 
 const Subscribe = () => {
   return (
@@ -10,25 +11,17 @@ const Subscribe = () => {
           <input type='text' placeholder='ایمیل خود را وارد کنید...'></input>
         </form>
         <div className='socials'>
-          <div className='social-icon'>
-           
-            <FaInstagram/>
-           
+          <div className='social-icon'> 
+            <FaInstagram/> 
           </div>
-          <div className='social-icon'>
-            
+          <div className='social-icon'>  
             <FaTwitter/>
-           
           </div>
-          <div className='social-icon'>
-           
+          <div className='social-icon'>  
             <FaFacebook/>
-            
           </div>
           <div className='social-icon'>
-       
             <TiSocialGooglePlus/>
-          
           </div>
         </div>
      </div>
